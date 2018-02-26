4847109cefd628284b696edf9e57ded9ca04e000
L. D. Nicolas May <LDNicolasMay@gmail.com>
2018-02-26 13:16:36 -0500

    clean up code a little; clarify some comments

--- UDS_Enrolled_Report_table.R
+++ UDS_Enrolled_Report_table.R
@@ -14,10 +14,10 @@ args = commandArgs(trailingOnly=TRUE)
 
 # Test if there is at least one argument: if not, return an error
 if (length(args) == 0) {
-  stop("At least one argument must be supplied: [UMMAPMindsetRegistryFile].csv", call.=FALSE)
+  stop("At least one argument must be supplied: [UMMAPMindsetRegistryFile].csv", call. = FALSE)
 } 
 # Maybe use this later if the user wants to name the output csv file
-# else if (length(args)==1) {
+# else if (length(args) == 1) {
 # # default output file
 # args[2] = "out.txt"
 #}
@@ -38,49 +38,41 @@ ms_reg <- ms_reg %>%
 
 # Fxn for outputting 
 single_grp_table <- function(x, group_var) {
-  # uds_dx_df <- data_frame(UDS_dx = unique(x$UDS_dx))
   distinct_grp_vals <- distinct(x, !!group_var)
   x %>% 
     group_by(!!group_var) %>% 
     summarize(Count = n()) %>%
-    # right_join(uds_dx_df, by = "UDS_dx") %>%
     right_join(distinct_grp_vals) %>%
     arrange(!!group_var)
 }
 # Fxn for outputting tables with one group variable and one filter variable
 single_grp_filter_table <- function(x, group_var, filter_var, filter_var_string) {
-  # uds_dx_df <- data_frame(UDS_dx = unique(x$UDS_dx))
   distinct_grp_vals <- distinct(x, !!group_var)
   x %>% 
     group_by(!!group_var) %>% 
     filter(!!filter_var == filter_var_string) %>% 
     summarize(Count = n()) %>% 
-    # right_join(uds_dx_df, by = "UDS_dx") %>% 
     right_join(distinct_grp_vals) %>% 
     arrange(!!group_var)
 }
 # Fxn for outputting tables with two group variables
 double_grp_table <- function(x, group_var_1, group_var_2) {
-  # uds_dx_df <- data_frame(UDS_dx = unique(x$UDS_dx))
   distinct_grp_vals <- distinct(x, !!group_var_1)
   x %>% 
     group_by(!!group_var_1, !!group_var_2) %>% 
     summarize(Count = n()) %>% 
     spread(!!group_var_2, Count) %>% 
-    # right_join(uds_dx_df, by = "UDS_dx") %>%
     right_join(distinct_grp_vals) %>%
     arrange(!!group_var_1)
 }
 # Fxn for outputting tables with three group variables
 triple_grp_table <- function(x, group_var_1, group_var_2, group_var_3) {
-  # uds_dx_df <- data_frame(UDS_dx = unique(x$UDS_dx))
   distinct_grp_vals <- distinct(x, !!group_var_1)
   x %>% 
     group_by(!!group_var_1, !!group_var_2, !!group_var_3) %>% 
     summarize(Count = n()) %>% 
     unite(col = United, !!group_var_2, !!group_var_3, sep = "_") %>% 
     spread(United, Count) %>% 
-    # right_join(uds_dx_df, by = "UDS_dx") %>% 
     right_join(distinct_grp_vals) %>%
     arrange(!!group_var_1)
 }
@@ -147,13 +139,13 @@ totals <- vapply(X = big_tbl[, 2:ncol(big_tbl)],
                  FUN = sum, na.rm = TRUE, FUN.VALUE = numeric(1))
 total_row <- as_data_frame(matrix(c("Totals", totals), nrow = 1, byrow = TRUE))
 names(total_row) <- names(big_tbl)
-# total_row
+# total_row # Test printout
 # Attach totals row
 big_tbl <- rbind(big_tbl, total_row)
-# Make integer columns integers
+# Coerce integer columns to integers
 big_tbl[2:ncol(big_tbl)] <- lapply(X = big_tbl[2:ncol(big_tbl)], 
                                    FUN = as.integer)
-# big_tbl
+# big_tbl # Test printout
 
 # Build proportions row
 # big_tbl$UDS_dx == "Totals"
@@ -163,14 +155,14 @@ get_proportion <- function(x) {
 }
 proportions <- vapply(X = big_tbl[1:(nrow(big_tbl)-1), 2:ncol(big_tbl)], 
                       FUN = get_proportion, FUN.VALUE = numeric(1))
-# proportions
+# proportions # Test printout
 proportion_row <- as_data_frame(matrix(c("Proportions", proportions), nrow = 1, byrow = TRUE))
-# proportion_row
+# proportion_row # Test printout
 names(proportion_row) <- names(big_tbl)
-# proportions_row
-
+# proportions_row # Test printout
+# Attach proportions row
 big_tbl <- rbind(big_tbl, proportion_row)
-# big_tbl
+# big_tbl # Test printout
 
 # Rename headers (generalize this later)
 names(big_tbl) <- c("UDS dx", "Total Count",
