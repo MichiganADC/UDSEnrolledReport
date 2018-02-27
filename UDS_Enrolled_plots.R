1ad6c2563abee96e7dbbccbdf4b02cfc47f7a7d3
LDNicolasMay@gmail.com <hynsoosnyh@gmail.com>
2018-02-26 20:08:32 -0500

    figuring out scalable cumulative frequency plots with ggplot

--- UDS_Enrolled_plots.R
+++ UDS_Enrolled_plots.R
@@ -22,7 +22,8 @@ library(tidyverse)
 
 # Choose the csv file from the UMMAP Mindset Registry RC report
 # ms_reg_file <- args[1]
-ms_reg_file <- file.path("input_csv", "UMMAPMindsetRegistry_DATA_LABELS_2018-02-23_1042.csv")
+ms_reg_file <- 
+  file.path("input_csv", "UMMAPMindsetRegistry_DATA_LABELS_2018-02-23_1042.csv")
 ms_reg <- readr::read_csv(file = ms_reg_file, trim_ws = TRUE)
 
 names(ms_reg) <- 
@@ -50,8 +51,11 @@ max_date <- max(ms_reg$Exam_Date)
 # Plot cumulative participants by 'Exam_Date' -- All participants
 ggplot(ms_reg, aes(x = Exam_Date)) +
   geom_line(aes(len = nrow(ms_reg), y = len * ..y..), stat = "ecdf") +
-  scale_x_date(name = "Visit Date", date_labels = "%b %y", date_breaks = "1 month") +
-  scale_y_continuous(name = "Cumulative Participants", breaks = seq(0, nrow(ms_reg) + 10, by = 10)) + 
+  scale_x_date(name = "Visit Date", 
+               date_labels = "%b %y", 
+               date_breaks = "1 month") +
+  scale_y_continuous(name = "Cumulative Participants", 
+                     breaks = seq(0, nrow(ms_reg) + 10, by = 10)) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ggtitle(label = "Total Participants Over Time")
 ggsave("plots/UDS_Enrolled_plot-Total_participants.png", width = 6, height = 4)
@@ -65,8 +69,11 @@ ms_reg <- ms_reg %>%
                             ifelse(Sex == "Male", sex_ct$n[2], NA)))
 ggplot(ms_reg, aes(x = Exam_Date, color = Sex)) +
   geom_line(aes(len = Sex_Count, y = ..y.. * len), stat = "ecdf") + 
-  scale_x_date(name = "Visit Date", date_labels = "%b %y", date_breaks = "1 month") +
-  scale_y_continuous(name = "Cumulative Participants", breaks = seq(0, nrow(ms_reg) + 10, by = 10)) + 
+  scale_x_date(name = "Visit Date", 
+               date_labels = "%b %y", 
+               date_breaks = "1 month") +
+  scale_y_continuous(name = "Cumulative Participants", 
+                     breaks = seq(0, nrow(ms_reg) + 10, by = 10)) + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ggtitle(label = "Participants Over Time by Sex")
 ggsave("plots/UDS_Enrolled_plot-Participants_by_sex.png", width = 6, height = 4)
@@ -82,8 +89,40 @@ ms_reg <- ms_reg %>%
   filter(!is.na(Race))
 ggplot(ms_reg, aes(x = Exam_Date, color = Race)) +
   geom_line(aes(len = Race_Count, y = ..y.. * len), stat = "ecdf") + 
-  scale_x_date(name = "Visit Date", date_labels = "%b %y", date_breaks = "1 month") +
-  scale_y_continuous(name = "Cumulative Participants", breaks = seq(0, nrow(ms_reg) + 10, by = 10)) +
+  scale_x_date(name = "Visit Date", 
+               date_labels = "%b %y", 
+               date_breaks = "1 month") +
+  scale_y_continuous(name = "Cumulative Participants", 
+                     breaks = seq(0, nrow(ms_reg) + 10, by = 10)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ggtitle(label = "Participants Over Time by Race")
 ggsave("plots/UDS_Enrolled_plot-Participants_by_race.png", width = 6, height = 4)
+
+# There has to be a better/scalable way to create these 
+# Cumulative Frequency Plots ...
+# maybe https://stackoverflow.com/questions/18379933/plotting-cumulative-counts-in-ggplot2
+# Empirical Cumulative Distribution Function
+df <- data.frame(
+  x = c(rnorm(100, 0, 3), rnorm(100, 0, 10)),
+  g = gl(2, 100),
+  u = rep(1, 200),
+  n = seq(1, 200)
+)
+cumsum(df$u)
+ggplot(df, aes(x = n, y = cumsum(u), color = g)) + geom_line()
+
+
+
+
+
+
+
+
+
+#####################
+#####################
+#####################
+#### EXTRA SPACE ####
+#####################
+#####################
+#####################
